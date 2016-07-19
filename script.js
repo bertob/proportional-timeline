f2235c91254b4ea6253b5bc969b246f70dbb58e5
Tobias Bernard <tobias@zenbook>
2016-07-19 22:44:10 +0200

    [refactor] switch to draggability for smoother movement

--- script.js
+++ script.js
@@ -1,73 +1,64 @@
-var TOUCHPAD_SIZE = 464;
+var MINS_PER_ROW = 20;
 
-var hoverX, hoverY;
-var moving = false;
-var offsetX = 0;
+var moved = false;
+var movedDistance = 0;
 
-var progress = 45; // in minutes
+// in minutes
+var progress = 45;
 var length = 78;
-var MINUTES_PER_ROW = 20;
 
-document.ontouchmove = function(e) {
-  e.preventDefault();
-};
+updateProgressbar(length, progress);
+update();
 
-$("#scrubber").on("move", function(e) {
-  // console.log(e.pageX);
-  if (!moving) {
-    moving = true;
-    var rect = $("#scrubber").get(0).getBoundingClientRect();
-    offsetX = e.pageX - rect.left;
-    hover(e.pageX);
-  }
-  else {
-    hover(e.pageX);
-  }
-});
-$("#scrubber").on("moveend", function(e) {
-  move(0, 0);
-  moving = false;
-});
-
-function hover(pX) {
-  var rect = $("#scrubber").get(0).getBoundingClientRect();
-  hoverX = (pX - rect.left) - offsetX;
-  move(hoverX, hoverY);
-}
-function move(x, y) {
-  // console.log(x);
-  if (Math.abs(x) < 5) x = 0;
-  changeProgress(x);
-  $("#scrubber").css({"transform": "translate(" + x + "px, " + 0 + "px)"});
+function update() {
+  if (moved)
+    changeProgress();
+  setTimeout(function() {
+    update();
+  }, 20);
 }
 
-function changeProgress(speed) {
-  newProgress = progress + Math.pow(speed * 0.005, 2) * sign(speed);
+var $draggable = $('#scrubber').draggabilly({
+  axis: "x",
+  containment: "#scrubber-container"
+});
+
+$draggable.on("dragStart", function(event, pointer) {
+  moved = true;
+});
+$draggable.on("dragMove", function(event, pointer, moveVector) {
+  movedDistance = moveVector.x;
+});
+$draggable.on("dragEnd", function(event, pointer) {
+  changeProgress(0);
+  moved = false;
+  $draggable.css("left", "0");
+});
+
+function changeProgress() {
+  newProgress = progress + (Math.pow(10, Math.abs(movedDistance)/150) * sign(movedDistance)) / 90;
   progress = Math.max(Math.min(newProgress, length), 0);
-  // console.log(speed, progress);
-
   updateProgressbar(length, progress);
 }
 function updateProgressbar(length, progress) {
-  var bars = Math.ceil(length/20);
-  var completeBars = Math.floor(progress/20);
-  var maxPercentage = (length % 20) * 100/20;
+  var bars = Math.ceil(length / MINS_PER_ROW);
+  var completeBars = Math.floor(progress / MINS_PER_ROW);
+  var maxPercentage = (length % MINS_PER_ROW) * 100 / MINS_PER_ROW;
   for (var i=1; i<=bars; i++) {
     if (i <= completeBars) {
       $("#bar-" + i).children(".active").css("width", "100%");
     }
-    else if (progress > (i-1)*20 && progress < ((i-1)+1)*20) {
-      barProgress = (progress % 20) * 100/20;
-      $("#bar-" + i).children(".active").css("width", Math.min(barProgress, maxPercentage) + "%");
-      console.log(barProgress, maxPercentage);
-      console.log(Math.min(barProgress, maxPercentage));
+    else if (progress > (i-1) * MINS_PER_ROW && progress < ((i-1)+1) * MINS_PER_ROW) {
+      barProgress = (progress % MINS_PER_ROW) * 100 / MINS_PER_ROW;
+      if (i === bars)
+        $("#bar-" + i).children(".active").css("width", Math.min(barProgress, maxPercentage) + "%");
+      else
+        $("#bar-" + i).children(".active").css("width", barProgress + "%");
     }
     else {
       $("#bar-" + i).children(".active").css("width", "0");
     }
   }
-
 }
-updateProgressbar(length, progress);
 
 function sign(x) { return x > 0 ? 1 : x < 0 ? -1 : 0; }
